80170841d8747e1c3d321d8c9309b756c73ef755
Michael Davis <michael@damaru.com>
2019-12-09 14:27:08 -0500

    Improved queue support.

--- src/main/java/com/solace/asyncapi/OrderChannel.java
+++ src/main/java/com/solace/asyncapi/OrderChannel.java
@@ -1,5 +1,4 @@
 
-
 package com.solace.asyncapi;
 import javax.annotation.PostConstruct;
 
@@ -8,14 +7,18 @@ import org.springframework.stereotype.Component;
 
 import com.solacesystems.jcsmp.BytesXMLMessage;
 import com.solacesystems.jcsmp.DeliveryMode;
+
+import com.solacesystems.jcsmp.EndpointProperties;
+
+import com.solacesystems.jcsmp.JCSMPErrorResponseException;
+import com.solacesystems.jcsmp.JCSMPErrorResponseSubcodeEx;
 import com.solacesystems.jcsmp.JCSMPException;
 import com.solacesystems.jcsmp.JCSMPFactory;
 import com.solacesystems.jcsmp.JCSMPSession;
 import com.solacesystems.jcsmp.JCSMPStreamingPublishEventHandler;
-import com.solacesystems.jcsmp.TextMessage;
-import com.solacesystems.jcsmp.Topic;
-import com.solacesystems.jcsmp.XMLMessageConsumer;
-import com.solacesystems.jcsmp.XMLMessageListener;
+
+import com.solacesystems.jcsmp.Queue;
+import com.solacesystems.jcsmp.TextMessage;import com.solacesystems.jcsmp.Topic;
 import com.solacesystems.jcsmp.XMLMessageProducer;
 
 @Component
@@ -23,7 +26,6 @@ public class OrderChannel {
 
     // Channel name: solace/order/{action}/1/{trace}/{span}
     private static final String PUBLISH_TOPIC = "solace/order/%s/1/%s/%d";
-    private static final String SUBSCRIBE_TOPIC = "solace/order/*/1/*/*";
 
     @Autowired
     private SolaceSession solaceSession;
@@ -31,7 +33,6 @@ public class OrderChannel {
     private Serializer<Order> serializer;
     private TextMessage textMessage = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
     private XMLMessageProducer producer;
-    XMLMessageConsumer consumer;
 
     @PostConstruct
     public void init() throws Exception {
@@ -46,13 +47,6 @@ public class OrderChannel {
         textMessage.setDeliveryMode(DeliveryMode.DIRECT);
     }
 
-    public void subscribe(SubscribeListener listener) throws Exception {
-    	MessageListener messageListener = new MessageListener(listener);
-    	consumer = jcsmpSession.getMessageConsumer(messageListener);
-    	Topic topic = JCSMPFactory.onlyInstance().createTopic(SUBSCRIBE_TOPIC);
-    	jcsmpSession.addSubscription(topic);
-    	consumer.start();
-    }
 
     private Topic formatTopic(Action action, String trace, int span) {
         String topicString = String.format(PUBLISH_TOPIC, action, trace, span);
@@ -77,50 +71,12 @@ public class OrderChannel {
 
     public void close() {
 
-        if (consumer != null) {
-            consumer.close();        
-        }
-
         solaceSession.close();
-
     }
 
 
     public static enum Action { buyItem,returnItem }
 
-    public interface SubscribeListener {
-        public void onReceive(OrderMessage orderMessage);
-        public void handleException(Exception exception);
-    }
-    
-    class MessageListener implements XMLMessageListener {
-
-        SubscribeListener listener;
-        
-        public MessageListener(SubscribeListener listener) {
-            this.listener = listener;
-        }
-        
-        @Override
-        public void onException(JCSMPException exception) {
-            listener.handleException(exception);
-        }
-
-        @Override
-        public void onReceive(BytesXMLMessage bytesMessage) {
-            TextMessage textMessage = (TextMessage) bytesMessage;
-            Order payload;
-            try {
-                payload = serializer.deserialize(textMessage.getText());
-                OrderMessage  orderMessage = new OrderMessage();
-                orderMessage.setPayload(payload);
-                orderMessage.setTopic(bytesMessage.getDestination().getName());
-                listener.onReceive(orderMessage);
-            } catch (Exception exception) {
-                listener.handleException(exception);
-            }            
-        }
-    }
 
     public interface PublishListener {
         public void onResponse(String messageId);
@@ -146,13 +102,20 @@ public class OrderChannel {
         }
     }
 
-    public void setupQueue() {
+    public void provisionQueue() throws Exception {
         final Queue queue = JCSMPFactory.onlyInstance().createQueue("orderQueue");
         final EndpointProperties endpointProps = new EndpointProperties();
         endpointProps.setPermission(EndpointProperties.PERMISSION_DELETE);
-        endpointProps.setAccessType(EndpointProperties.ACCESSTYPE_EXCLUSIVE);
+        endpointProps.setAccessType(EndpointProperties.ACCESSTYPE_NONEXCLUSIVE);
         jcsmpSession.provision(queue, endpointProps, JCSMPSession.FLAG_IGNORE_ALREADY_EXISTS);
-        
+        try {
+            Topic topic = JCSMPFactory.onlyInstance().createTopic("solace/order/*/1/*/*");
+            jcsmpSession.addSubscription(queue, topic, JCSMPSession.WAIT_FOR_CONFIRM);
+        } catch (JCSMPErrorResponseException ex) {
+            if (ex.getSubcodeEx() != JCSMPErrorResponseSubcodeEx.SUBSCRIPTION_ALREADY_PRESENT) {
+                throw ex;
+            }
+        }
     }
 
 }
---
+++ src/main/java/com/solace/asyncapi/OrderQueueChannel.java
@@ -0,0 +1,106 @@
+
+package com.solace.asyncapi;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.solacesystems.jcsmp.BytesXMLMessage;
+import com.solacesystems.jcsmp.ConsumerFlowProperties;
+
+import com.solacesystems.jcsmp.EndpointProperties;
+import com.solacesystems.jcsmp.FlowReceiver;
+import com.solacesystems.jcsmp.JCSMPException;
+import com.solacesystems.jcsmp.JCSMPFactory;
+import com.solacesystems.jcsmp.JCSMPSession;
+
+import com.solacesystems.jcsmp.Queue;
+import com.solacesystems.jcsmp.TextMessage;import com.solacesystems.jcsmp.XMLMessageListener;
+
+
+@Component
+public class OrderQueueChannel {
+
+    // Channel name: orderQueue
+
+    @Autowired
+    private SolaceSession solaceSession;
+    private JCSMPSession jcsmpSession;
+    private Serializer<Order> serializer;
+    private FlowReceiver flowReceiver;
+
+    @PostConstruct
+    public void init() throws Exception {
+        jcsmpSession = solaceSession.getSession();
+        serializer = SerializerFactory.getSerializer("application/json", Order.class);
+        provisionQueue();
+    }
+
+
+
+    public void subscribe(SubscribeListener listener) throws Exception {
+    	MessageListener messageListener = new MessageListener(listener);
+        final Queue queue = JCSMPFactory.onlyInstance().createQueue("orderQueue");
+        ConsumerFlowProperties flowProps = new ConsumerFlowProperties();
+        flowProps.setEndpoint(queue);
+    	flowReceiver = jcsmpSession.createFlow(messageListener, flowProps);
+    	flowReceiver.start();
+    }
+
+
+
+    public void close() {
+
+        if (flowReceiver != null) {
+            flowReceiver.close();        
+        }
+
+        solaceSession.close();
+    }
+
+
+    public interface SubscribeListener {
+        public void onReceive(OrderMessage orderMessage);
+        public void handleException(Exception exception);
+    }
+    
+    class MessageListener implements XMLMessageListener {
+
+        SubscribeListener listener;
+        
+        public MessageListener(SubscribeListener listener) {
+            this.listener = listener;
+        }
+        
+        @Override
+        public void onException(JCSMPException exception) {
+            listener.handleException(exception);
+        }
+
+        @Override
+        public void onReceive(BytesXMLMessage bytesMessage) {
+            TextMessage textMessage = (TextMessage) bytesMessage;
+            Order payload;
+            try {
+                payload = serializer.deserialize(textMessage.getText());
+                OrderMessage  orderMessage = new OrderMessage();
+                orderMessage.setPayload(payload);
+                orderMessage.setTopic(bytesMessage.getDestination().getName());
+                listener.onReceive(orderMessage);
+            } catch (Exception exception) {
+                listener.handleException(exception);
+            }            
+        }
+    }
+
+    public void provisionQueue() throws Exception {
+        final Queue queue = JCSMPFactory.onlyInstance().createQueue("orderQueue");
+        final EndpointProperties endpointProps = new EndpointProperties();
+        endpointProps.setPermission(EndpointProperties.PERMISSION_DELETE);
+        endpointProps.setAccessType(EndpointProperties.ACCESSTYPE_NONEXCLUSIVE);
+        jcsmpSession.provision(queue, endpointProps, JCSMPSession.FLAG_IGNORE_ALREADY_EXISTS);
+        
+    }
+
+}
+
